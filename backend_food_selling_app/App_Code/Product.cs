50122058ef62ab1fb45330b2bc634c923f3b1fb4
lele9h0st <hoangtranhuy2001@gmail.com>
2022-07-16 09:25:42 +0700

    first commit branch hoang

---
+++ backend_food_selling_app/App_Code/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Product
+/// </summary>
+[Serializable]
+public class Product
+{
+    public int masp { get; set; }
+    public string tensp { get; set; }
+    public double gia { get; set; }
+    public int soluongmua { get; set; }
+    public string ngaydathang { get; set; }
+}
---
+++ backend_food_selling_app/model/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace backend_food_selling_app.model
+{
+    [Serializable]
+    public class Product
+    {
+        public int masp { get; set; }
+        public string tensp { get; set; }
+        public double gia { get; set; }
+        public int soluongmua { get; set; }
+        public string ngaydathang { get; set; }
+    }
+}
