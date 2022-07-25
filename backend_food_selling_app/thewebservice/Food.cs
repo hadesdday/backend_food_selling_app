c7861fcdf87fdae364a403e17a4dfa18b3d1e157
NguyenVanTruong2001 <19130256@st.hcmuaf.edu.vn>
2022-07-25 15:55:25 +0700

    backend commit

---
+++ backend_food_selling_app/thewebservice/Food.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace thewebservice
+{
+    [Serializable]
+    public class Food
+    {
+        public int FoodId { get; set; }
+        public string FoodName { get; set; }
+        public string FoodImage { get; set; }
+        public string FoodDescription { get; set; }
+        public int FoodPrice { get; set; }
+        public string FoodTypeId { get; set; }
+
+    }
+}
