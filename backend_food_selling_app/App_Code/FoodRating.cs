0b2e923656cfb7574bf59e7610de4a346295fb89
NguyenVanTruong2001 <19130256@st.hcmuaf.edu.vn>
2022-07-31 04:59:21 +0700

    put cs files and methods to appcode folder and the web services

---
+++ backend_food_selling_app/App_Code/FoodRating.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+    [Serializable]
+    public class FoodRating
+    {
+        public int RateId { get; set; }
+        public string FoodId { get; set; }
+        public double FoodRate { get; set; }
+        public string FoodComment { get; set; }
+    }
---
+++ backend_food_selling_app/App_Code/FoodType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+    [Serializable]
+    public class FoodType
+    {
+        public int FoodTypeId { get; set; }
+        public string FoodTypeName { get; set; }
+    }
